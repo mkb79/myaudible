00e06ecb5a938b509d251aae44862dd4931ee291
mkb79 <mkb79@hackitall.de>
2021-10-01 12:49:18 +0200

    rename path to add a new device

--- devices/urls.py
+++ devices/urls.py
@@ -5,9 +5,9 @@ from . import views
 
 urlpatterns = [
     path('dashboard/', views.RegisterDeviceView.as_view(), name='dashboard'),
-    path('register-device/', views.RegisterDeviceView.as_view(), name='audible_add_device'),
-    path('register-device/<uuid:login_uuid>/<path:resource>', views.register_device),
-    path('register-device/<uuid:login_uuid>/', views.register_device),
+    path('add-device/', views.RegisterDeviceView.as_view(), name='audible_add_device'),
+    path('add-device/<uuid:login_uuid>/<path:resource>', views.register_device),
+    path('add-device/<uuid:login_uuid>/', views.register_device),
     path('import-file/', views.ImportAuthFileView.as_view(), name='import_auth_file'),
     path('<int:pk>/', views.OwnDevicesDetailView.as_view(), name='own_device_detail'),
     path('', views.OwnDevicesListView.as_view(), name='own_devices_list')
